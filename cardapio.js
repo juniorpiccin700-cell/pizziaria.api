639ba9d11a2ec3b523e997acc77d4f0ddc27b0f1
célula <juniorpiccin700@gmail.com>
2026-09-03 11:42:06 -0300

    Primeira rota

---
+++ cardapio.js
@@ -0,0 +1,5 @@
+export let pizzas = [
+    {id: 1, sabor: 'Calabresa', preco: 45.90},
+    {id: 2, sabor: 'Frango', preco: 55.90},
+     {id:3, sabor: 'Ovo', preco: 40.90}
+]
--- index.js
+++ index.js
@@ -1,7 +1,37 @@
-import { mensagem } from "./aula.js";
+import { pizzas } from "./cardapio.js";
 
-mensagem ('Junior')
+import 'dotenv/config';
+import express from 'express';
 
-import chalk from 'chalk';
+const app = express()
 
-console.log(chalk.green('Bem vindo'));
+app.use(express.json())
+
+app.get('/', (req, res) => {
+
+    res.json({ message: "Bem-vindo à API da pizzaria Senac!" });
+});
+
+const PORTA = process.env.PORT
+
+app.listen(PORTA, ()=>{
+    console.log(`Servidor rodando na porta ${PORTA}`)
+});
+app.get('/pizzas', (req, res) => {
+
+    res.json(pizzas);
+})
+app.get('/pizzas/:id',(req, res)=>{
+    const id = parseInt(req.params.id);
+    const pizza = pizzas.find(p => p.id === id);
+    if (!pizza) {
+        return res.status(404).json({ error: 'Pizza não encontrada' });
+    }
+ 
+    res.json(pizza);
+});
+ 
+
+
+
+
