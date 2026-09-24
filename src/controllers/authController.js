0ee06c1f3ecbc182546daee01f39ad0333810546
célula <juniorpiccin700@gmail.com>
2026-09-24 11:29:26 -0300

    Finalizando a Api 1

---
+++ src/controllers/authController.js
@@ -0,0 +1,21 @@
+// src/controllers/authController.js
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import * as clienteServices from '../services/clienteServices.js';
+export const login = async (req, res) => {
+const { cpf, senha } = req.body;
+try {
+    const cliente = await clienteServices.findAll(cpf);
+    if (!cliente )
+        return res.status(401).json({ mensagem: 'Credenciais inválidas.' });
+    const senhaValida = await bcrypt.compare(senha, cliente[0].senha);
+    if (!senhaValida)
+        return res.status(401).json({ mensagem: 'Credenciais inválidas.' });
+    const payload = {cpf: cliente.cpf, email: cliente.email};
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ mensagem: 'Login realizado com sucesso.', token: token });}
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ mensagem: 'Erro interno do servidor.' });
+    }
+}
--- src/routers/clienteRouters.js
+++ src/routers/clienteRouters.js
@@ -4,10 +4,10 @@ import validate from '../middlewares/validate.js';
 import { clienteCreateSchema, clienteUpdateSchema } from '../controllers/clienteController.js';
 
 const router = express.Router();
-//import authMiddleware from '../middlewares/authMiddleware.js';
+import authMiddleware from '../middlewares/authMiddleware.js';
 
 router.post('/', validate(clienteCreateSchema), clienteController.adicionarCliente);
-//router.use(authMiddleware); // Aplica o middleware de autenticação para todas as rotas abaixo
+router.use(authMiddleware); // Aplica o middleware de autenticação para todas as rotas abaixo
 
 router.get('/', clienteController.listarClientes);
 router.put('/:cpf', validate(clienteUpdateSchema), clienteController.atualizarCliente);
